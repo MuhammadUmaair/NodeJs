ecb4f2ee653b0de679752c0d81794ab44202a28e
Muhammad Umair <mohammad.umair9610@gmail.com>
2024-02-29 17:32:25 +0500

    comment console.log messages

--- understanding-the-basic/app.js
+++ understanding-the-basic/app.js
@@ -13,18 +13,18 @@ const http = require("http"); //to find global file
 
 const server = http.createServer((req, res) => {
   //Arrow function
-  console.log(req); // we received lots of data
-  console.log(req.url); //if we need url
-  console.log(req.method); //if we need methods
-  console.log(req.headers); //if we need headers
+  //   console.log(req); // we received lots of data
+  //   console.log(req.url); //if we need url
+  //   console.log(req.method); //if we need methods
+  //   console.log(req.headers); //if we need headers
   //   for return response we comment exit
   //process.exit(); // if exit after execute
 
   res.setHeader("Content-Type", "text/html");
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  
-  res.write('</html>');
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+
+  res.write("</html>");
 });
 server.listen(3000);
