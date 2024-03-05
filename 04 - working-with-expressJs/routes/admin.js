4878df92992011d1e73293ea6408e172f26966b4
Muhammad Umair <mohammad.umair9610@gmail.com>
2024-03-05 19:48:18 +0500

    add '/admin' before admin routes

--- 04 - working-with-expressJs/routes/admin.js
+++ 04 - working-with-expressJs/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/add-product", (req, res, next) => {
   // Send a form to add a product
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 
   //request can't continue journey, so we add next function that allows the request to continue to the next middleware in line
