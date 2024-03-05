d2a703a661a9c31b2cae402047bf762f314c6f33
Muhammad Umair <mohammad.umair9610@gmail.com>
2024-03-05 20:10:50 +0500

    create and serving html pages

--- 04 - working-with-expressJs/routes/admin.js
+++ 04 - working-with-expressJs/routes/admin.js
@@ -1,19 +1,18 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 // Route for adding a product (GET request)
 router.get("/add-product", (req, res, next) => {
   // Send a form to add a product
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 
   //request can't continue journey, so we add next function that allows the request to continue to the next middleware in line
   //   next();
 });
 
 // Route for handling product addition (POST request)
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   // Log the incoming data from the form
   console.log(req.body);
   // Redirect to the home page
