ec8aa213eb975cabd01c2fbbccb45b3694cffe9f
Muhammad Umair <mohammad.umair9610@gmail.com>
2024-03-06 18:28:17 +0500

    add admin routes

--- 05 - working-with-dynamic-content-and-adding-templating-engines/routes/shop.js
+++ 05 - working-with-dynamic-content-and-adding-templating-engines/routes/shop.js
@@ -2,10 +2,12 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 const router = express.Router();
 
 // Route for the home page
 router.get("/", (req, res, next) => {
+  console.log('adminData',adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
